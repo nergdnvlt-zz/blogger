5c3afc0b501d0754ed48f961c71ee273c263c5fe
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-22 17:26:59 -0600

    Setting up articles controller.

--- spec/features/articles1_user_sees_all_articles_spec.rb
+++ spec/features/articles1_user_sees_all_articles_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe 'Article Index Page' do
   context 'a user visits /articles' do
-    it 'and the page displays all the articles'
-    article1 = Article.create!(title: 'The Dead', body: 'They come back to life.')
-    article2 = Article.create!(title: 'The Dead 2', body: 'They come back to life again.')
+    it 'and the page displays all the articles' do
+      article1 = Article.create!(title: 'The Dead', body: 'They come back to life.')
+      article2 = Article.create!(title: 'The Dead 2', body: 'They come back to life again.')
 
-    visit '/articles'
+      visit '/articles'
 
-    expect(page).to have_content(article1.title)
-    expect(page).to have_content(article1.body)
-    expect(page).to have_content(article2.title)
-    expect(page).to have_content(article2.body)
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article1.body)
+      expect(page).to have_content(article2.title)
+      expect(page).to have_content(article2.body)
+    end
   end
 end
