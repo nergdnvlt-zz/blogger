20a16e9b578f3c7f2170be7217700f7d756dca37
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-22 17:48:28 -0600

    Working on building out the index view.

--- spec/features/articles1_user_sees_all_articles_spec.rb
+++ spec/features/articles1_user_sees_all_articles_spec.rb
@@ -2,16 +2,38 @@ require 'rails_helper'
 
 describe 'Article Index Page' do
   context 'a user visits /articles' do
-    it 'and the page displays all the articles' do
-      article1 = Article.create!(title: 'The Dead', body: 'They come back to life.')
-      article2 = Article.create!(title: 'The Dead 2', body: 'They come back to life again.')
+    before(:each) do
+      @article1 = Article.create!(title: 'The Dead',
+                                  body: 'They come back to life.')
+      @article2 = Article.create!(title: 'The Dead 2',
+                                  body: 'They come back to life again.')
+    end
 
+    after(:each) do
+      DatabaseCleaner.clean
+    end
+
+    it 'and the page displays all the articles' do
       visit '/articles'
 
-      expect(page).to have_content(article1.title)
-      expect(page).to have_content(article1.body)
-      expect(page).to have_content(article2.title)
-      expect(page).to have_content(article2.body)
+      expect(page).to have_content(@article1.title)
+      expect(page).to have_content(@article1.body)
+      expect(page).to have_content(@article2.title)
+      expect(page).to have_content(@article2.body)
+    end
+
+    it 'and the page displays article titles as links' do
+      visit '/articles'
+
+      expect(page).to have_link(@article1.title)
+      expect(page).to have_link(@article2.title)
+    end
+
+    it 'and the page has a new article link' do
+      visit '/articles'
+
+      expect(page).to have_link('Create a New Article')
+      expect(page).to have_link(@article2.title)
     end
   end
 end
