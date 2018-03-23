9af8825a27b2c8650a1f8347a502a8463d57f9f0
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-22 22:12:10 -0600

    Calling it a night.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 # Controller for the Article Class
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -18,8 +18,15 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
     redirect_to articles_path
   end
 
@@ -27,5 +34,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
---
+++ spec/features/articles5_user_can_edit_an_article_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Article Edit Page' do
+  describe 'the user edits from the articles show page' do
+    it 'edits the article and shows it after' do
+      article1 = Article.create!(title: 'Billy Bob', body: 'BLAH BLAH BLAH')
+
+      visit article_path(article1)
+      click_link 'Edit'
+
+      fill_in 'article[title]', with: 'Edited Title!'
+      fill_in 'article[body]',  with: 'Edited Body!'
+
+      click_on 'Update Article'
+
+      expect(current_path).to eq(articles_path)
+
+      expect(page).to_not have_content('Billy Bob')
+      expect(page).to_not have_content('BLAH BLAH BLAH')
+      expect(page).to have_content(article2.title)
+      expect(page).to have_content(article2.body)
+    end
+  end
+end
