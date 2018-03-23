6a9df4aff7930359390284e57cf847216fe8f9d6
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-22 21:11:29 -0600

    Finished building a new article route and form.

---
+++ spec/features/articles3_user_can_create_new_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Article New Form Page' do
+  describe 'the user links from the articles index' do
+    it 'displays forms for a create new article' do
+      visit articles_path
+      click_link 'Create a New Article'
+
+      expect(current_path).to eq(new_article_path)
+
+      fill_in 'article[title]', with: 'New Title!'
+      fill_in 'article[body]',  with: 'New Body!'
+      click_on 'Create Article'
+      
+      expect(page).to have_content('New Title!')
+      expect(page).to have_content('New Body!')
+    end
+  end
+end
