716f942097514ff9ebf7b4bda52729da6e5efd2e
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-22 21:32:09 -0600

    Adding delete method to CRUD rails functionality.

---
+++ spec/features/articles4_user_can_delete_an_article_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Article Delete Page' do
+  describe 'the user links from the articles index' do
+    it 'displays all the articles without the deleted one' do
+      article1 = Article.create!(title: 'Billy Bob', body: 'BLAH BLAH BLAH')
+      article2 = Article.create!(title: 'Kabachi', body: 'HOOAH')
+
+      visit article_path(article1)
+      click_link 'Delete'
+
+      expect(current_path).to eq(articles_path)
+
+      expect(page).to_not have_content(article1.body)
+      expect(page).to_not have_content(article1.body)
+      expect(page).to have_content(article2.title)
+      expect(page).to have_content(article2.body)
+    end
+  end
+end
